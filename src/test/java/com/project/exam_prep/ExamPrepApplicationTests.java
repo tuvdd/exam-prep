037cede17d0015bd0ca1fd9057811212bb12aff7
Vuong Duy Duc Tu <112555576+tuvdd@users.noreply.github.com>
2024-10-02 10:22:21 +0700

    first commit

---
+++ src/test/java/com/project/exam_prep/ExamPrepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.exam_prep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamPrepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
